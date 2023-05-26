4e339e116134950c2a152f75495f267275faf808
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 16:49:02 -0400

    update components/component_education.js

--- components/component_education.js
+++ components/component_education.js
@@ -1,6 +1,5 @@
 import { P, T } from "../js/tools.js";
 import { Div, Label, addCI, Img } from "../js/tools.js";
-import { float_button } from "../js/float_button.js";
 
 export default function education(element, education) {
  
