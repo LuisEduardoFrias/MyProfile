64644ce56e467a836599831f95e8c760f6de81bb
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 16:23:55 -0400

    update components/component_reference.js

--- components/component_reference.js
+++ components/component_reference.js
@@ -11,7 +11,8 @@ export default function reference(element, proyect) {
  const img = img(",", "Imagen de titulo.", "");
  
  const divT = Div([], ".container-tegnology");
-
+ const divT = Div([], ".container-tegnology");
+ 
  proyect.tegnologys.forEach((element) => {
   divT.appendChild(Div([Label(element, ".tenology")], ".contan-tegnology"));
  });
