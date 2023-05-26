ad0c67b1214dd9b1a7492743b455115c3b3b951e
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 17:10:35 -0400

    update components/component_reference.js

--- components/component_reference.js
+++ components/component_reference.js
@@ -2,7 +2,7 @@ import { P, T } from "../js/tools.js";
 import { Div, Label, addCI, Img } from "../js/tools.js";
 import { float_button } from "../js/float_button.js";
 
-export default function reference(element, proyect) {
+export default function reference(element, reference) {
  
  const div = Div([], ".container-tegnology");
  
