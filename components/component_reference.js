7bcaf119af7a6742c5a58d5a338e5e954cdcc3ff
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 16:20:58 -0400

    update components/component_reference.js

--- components/component_reference.js
+++ components/component_reference.js
@@ -6,7 +6,7 @@ export default function reference(element, proyect) {
  
  const labelT = Label(proyect.tittle, ".tittle");
  const labelI = Label(proyect.description, ".description");
- const img = 
+ const img = img
  const divT = Div([], ".container-tegnology");
  const divR = Div([], ".container-repository");
 
