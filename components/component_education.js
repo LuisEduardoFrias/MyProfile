40b42776bbf1b76ddbd6008b8d72f332111ae414
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 16:28:56 -0400

    update components/component_education.js

--- components/component_education.js
+++ components/component_education.js
@@ -15,7 +15,7 @@ export default function education(element, proyect) {
   divE.appendChild( Div( [
       Label(e.tittle, ".tenology"),
       Label(e.institucion)
-      Img(e.tittle_img, "")
+      Img(e.tittle_img, "Imagen del titutl")
       ], ".contan-tegnology"));
  });
 
