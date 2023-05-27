282243194915d0b8a3a5cb8c11f79d949e792507
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 22:27:29 -0400

    update components/component_education.js

--- components/component_education.js
+++ components/component_education.js
@@ -14,7 +14,10 @@ export default function education(element, education) {
   contaS.appendChild( Div( [
       Label(e.tittle, ".sub_tittle"),
       A(e.url, e.institution, ".sub_institution"),
-      if(e.tittle_img === string.empt)
+      if(e.tittle_img !== "")
+      {
+       
+      }
       Img(e.tittle_img, "Imagen del titutlo", ".sub_img_tittle")
       ], ".sub_container_education"));
  });
