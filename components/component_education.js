b354cfbcbb0a7ca7e55de140234c94439177adf6
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-05-26 22:54:52 -0400

    update components/component_education.js

--- components/component_education.js
+++ components/component_education.js
@@ -13,7 +13,7 @@ export default function education(element, education) {
  education.more_education.forEach((e) => {
   contaS.appendChild( Div( [
       Label(e.tittle, ".sub_tittle"),
-      D
+      Div()
       Label("Institucion : ",".label_institution"),
       A(e.url, e.institution, ".sub_institution"),
       e.tittle_img !== "" ?
