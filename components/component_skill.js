a1c058d10d01f63a55f9f909d6c9aedbdf5e79c6
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-09 03:26:11 -0400

    update components/component_skill.js

--- components/component_skill.js
+++ components/component_skill.js
@@ -1,21 +1,26 @@
-import { P, S, T } from '../js/tools.js';
-
-import { source, Div, Img, Label } from '../js/tools.js';
+import { Colors, Ui } from "../js/tools.js";
 
 export default function skill(element, skill) {
+   const { P, T } = Colors;
 
-    const div = Div([ 
-         Img(skill.img, skill.name, ".skill-img", "width: 70%; height: 70%;"), 
-         Label(skill.name,".skill-name") 
-         ],".skill-container");
-    
-    div.style.borderBottomColor = `${P}`;
-    div.style.borderLeftColor = `${P}`;
-    div.style.borderTopColor = `${T}`;
-    div.style.borderRightColor = `${T}`;
-    
-    element.appendChild(div);
-    
-    return element;
+   element.appendChild(
+      Ui.Div(
+         [
+            Ui.Img(
+               skill.img,
+               skill.name,
+               ".skill-img",
+               `width: 70%; height: 70%;`
+            ),
+            Ui.Label(skill.name, ".skill-name"),
+         ],
+         ".skill-container",
+         `
+         border-bottom-color: ${P};
+         border-left-color: ${P};
+         border-top-color: ${T};
+         border-right-color: ${T};
+         `
+      )
+   );
 }
-
