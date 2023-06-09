1456310dd8c4cdb917a752860fe19b5095fa3153
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-09 03:22:02 -0400

    update components/component_experience.js

--- components/component_experience.js
+++ components/component_experience.js
@@ -1,33 +1,41 @@
-import { P, S, T } from '../js/tools.js';
-import { Div, Label } from '../js/tools.js';
+import { Colors, Ui } from "../js/tools.js";
 
 export default function experience(element, experience) {
+   const { P, T } = Colors;
+   const { Label, Div } = Ui;
 
-    const labelC = Label(experience.company, ".experience_company");
-    const labelD = Label(experience.descripcion, ".experience_description");
-    const labelP = Label(experience.position, ".experience_position");
-    
-    const divCT = Div([Label("logros/tareas", ".experience_titleTack")],".experience-container-logros-base");
-    
-    const divT = Div([], ".experience_container-tacks");    
-    
-    experience.tacks.forEach(element => 
-        divT.appendChild(Label(element.tack, ".experience_tack")))
-    
-    divT.style.border = "1px solid silver";
-  
-    divCT.appendChild(divT);
-    
-    const div = Div([ labelC, labelD, labelP, divCT], ".experience_constainer");
-   
-    div.style.borderWidth = "2px";
-    div.style.borderStyle = "double";
-    div.style.borderBottomColor = `${P}`;
-    div.style.borderLeftColor = `${P}`;
-    div.style.borderTopColor = `${T}`;
-    div.style.borderRightColor = `${T}`;
-    
-    element.appendChild(div);
-    
-    return element;
+   const labelC = Label(experience.company, ".experience_company");
+   const labelD = Label(experience.descripcion, ".experience_description");
+   const labelP = Label(experience.position, ".experience_position");
+
+   const divCT = Div(
+      [Label("logros/tareas", ".experience_titleTack")],
+      ".experience-container-logros-base"
+   );
+
+   const divT = Div(
+      [],
+      ".experience_container-tacks",
+      `border:1px solid silver;`
+   );
+
+   experience.tacks.forEach((element) =>
+      divT.appendChild(Label(element.tack, ".experience_tack"))
+   );
+
+   divCT.appendChild(divT);
+
+   element.appendChild(
+      Div(
+         [labelC, labelD, labelP, divCT],
+         ".experience_constainer",
+         `
+         border-width: 2px;
+         border-style: double;
+         border-bottom-color: ${P};
+         border-left-color: ${P};
+         border-top-color: ${T};
+         border-right-color: ${T};`
+      )
+   );
 }
