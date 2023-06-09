df84217fb002185ba3fc9caed3709726484c5e0e
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-09 03:25:37 -0400

    update components/component_reference.js

--- components/component_reference.js
+++ components/component_reference.js
@@ -1,21 +1,15 @@
-import { P, T } from "../js/tools.js";
-import { Div, Label } from "../js/tools.js";
+import { Colors, Ui } from "../js/tools.js";
 
 export default function reference(element, reference) {
+   const { P, T } = Colors;
+   const { Div, Label } = Ui;
 
- const labelT = Label(reference.name, ".name");
- const labelI = Label(reference.phone_number, ".phone");
- 
- const div = Div([labelT, labelI], ".constainer-reference");
- 
- div.style.borderWidth = "2px";
- div.style.borderStyle = "double";
- div.style.borderBottomColor = `${P}`;
- div.style.borderLeftColor = `${P}`;
- div.style.borderTopColor = `${T}`;
- div.style.borderRightColor = `${T}`;
-
- element.appendChild(div);
-
- return element;
+   element.appendChild(Div([Label(reference.name, ".name"), Label(reference.phone_number, ".phone")], ".constainer-reference",
+         `
+         border-width: 2px;
+         border-style: double;
+         border-bottom-color: ${P};
+         border-left-color: ${P};
+         border-top-color: ${T};
+         border-right-color: ${T};`));
 }
