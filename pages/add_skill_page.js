3c40e6997c2b5243e4985603237a426e7c213cef
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-09 03:38:13 -0400

    update pages/add_skill_page.js

---
+++ pages/add_skill_page.js
@@ -0,0 +1,21 @@
+import { Ui } from "../js/tools.js";
+
+export default function educati_page() {
+   const { Div, Label, Button, Input, Form } = Ui;
+   return Form(
+      [
+         Div(
+            [
+               Label("add new skill", ".tittle-section"),
+               Input("", "placeholder", "t", "error", "name"),
+               Input("", "placeholder", "t", "error", "name"),
+               Input("", "placeholder", "t", "error", "name"),
+               Input("", "placeholder", "t", "error", "name"),
+               Button("save", ".btn-section"),
+            ],
+            ".section"
+         ),
+      ],
+      ".setting-container-form"
+   );
+}
