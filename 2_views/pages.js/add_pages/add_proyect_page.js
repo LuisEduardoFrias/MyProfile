badfdc91d2c3177bc03fa4e8ea69e1ffb261704b
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-14 19:13:19 -0400

    update 2_views/pages.js/add_pages/add_proyect_page.js

--- 2_views/pages.js/add_pages/add_proyect_page.js
+++ 2_views/pages.js/add_pages/add_proyect_page.js
@@ -1,17 +1,13 @@
-import { Ui, handleUrl } from "../../../1_controllers/js/tools.js";
+import { Ui } from "../../../3_controllers/helpers/ui.js";
+import { handleUrl } from "../../../3_controllers/helpers/handleUrl.js";
+import { controller_ } from "../../../3_controllers/controller_";
 
 export default function addProyect() {
    const { Div, Label, Button, Input, Form } = Ui;
    return Div([
-      Button(
-         "back",
-         null,
-         `
-                  background-color:gray;
-                  border-radius:10px 10px;
-                  width:100px;
-                  height:35px;`,
-         (e) => handleUrl.back()
+      Div(
+         [Button("back", ".back-button", null, (e) => handleUrl.back())],
+         ".view-container-back-btn"
       ),
       Form(
          [
@@ -22,14 +18,8 @@ export default function addProyect() {
                   Input("", "placeholder", "t", "error", "name"),
                   Input("", "placeholder", "t", "error", "name"),
                   Input("", "placeholder", "t", "error", "name"),
-                  Button(
-                     "save",
-                     ".btn-section",
-                     `
-                  background-color:rgb(55,255,67);
-                  border-radius:10px 10px;
-                  width:100px;
-                  height:35px;`
+                  Button("save", ".btn-section", null, (e) =>
+                     controller_.post()
                   ),
                ],
                ".section"
