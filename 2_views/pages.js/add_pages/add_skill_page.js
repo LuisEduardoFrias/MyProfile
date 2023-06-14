c5564225336b042fc308551b510e6e90c01efa93
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-14 19:02:38 -0400

    update 2_views/pages.js/add_pages/add_skill_page.js

--- 2_views/pages.js/add_pages/add_skill_page.js
+++ 2_views/pages.js/add_pages/add_skill_page.js
@@ -3,16 +3,10 @@ import { Ui, handleUrl } from "../../../1_controllers/js/tools.js";
 export default function addSkill() {
    const { Div, Label, Button, Input, Form } = Ui;
    return Div([
-      Button(
-         "back",
-         null,
-         `
-                  background-color:gray;
-                  border-radius:10px 10px;
-                  width:100px;
-                  height:35px;`,
-         (e) => handleUrl.back()
-      ),
+      Div(
+            [Button("back", ".back-button", null, (e) => handleUrl.back())],
+            ".view-container-back-btn"
+         ),
       Form(
          [
             Div(
