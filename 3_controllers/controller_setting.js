c6d18ab7fdd3a9c283ef1f13cade90c85ef8f9c2
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:33:17 -0400

    update 3_controllers/controller_setting.js

---
+++ 3_controllers/controller_setting.js
@@ -0,0 +1,11 @@
+import { error_page } from "../../2_views/pages/error_page.js";
+import { setting_page } from "../../2_views/pages/setting_page.js";
+
+export class controllerSetting {
+   static setting() {
+      
+   }
+   static error() {
+      
+   }
+}
