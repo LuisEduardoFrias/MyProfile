a566c09af1eba81b93b826d8a07541409544bd96
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-14 19:15:30 -0400

    update 2_views/pages.js/add_pages/add_experience_page.js

--- 2_views/pages.js/add_pages/add_experience_page.js
+++ 2_views/pages.js/add_pages/add_experience_page.js
@@ -1,18 +1,15 @@
-import { Ui, handleUrl } from "../js/tools.js";
+import { Ui } from "../../../3_controllers/helpers/ui.js";
+import { handleUrl } from "../../../3_controllers/helpers/handleUrl.js";
+import { controller_experence } from "../../../3_controllers/controller_";
+
 
 export default function addExperience() {
    const { Div, Label, Button, Input, Form } = Ui;
    return Div([
-      Button(
-         "back",
-         null,
-         `
-                  background-color:gray;
-                  border-radius:10px 10px;
-                  width:100px;
-                  height:35px;`,
-         (e) => handleUrl.back()
-      ),
+      Div(
+            [Button("back", ".back-button", null, (e) => handleUrl.back())],
+            ".view-container-back-btn"
+         ),
       Form(
          [
             Div(
@@ -25,11 +22,8 @@ export default function addExperience() {
                   Button(
                      "save",
                      ".btn-section",
-                     `
-                  background-color:rgb(55,255,67);
-                  border-radius:10px 10px;
-                  width:100px;
-                  height:35px;`
+                     null,
+                     (e) =>  controller_reference.post()
                   ),
                ],
                ".section"
