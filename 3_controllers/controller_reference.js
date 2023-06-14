8a7b377e77c828df26da1e314b3954da685f55d8
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:32:34 -0400

    update 3_controllers/controller_reference.js

---
+++ 3_controllers/controller_reference.js
@@ -0,0 +1,11 @@
+import { add_reference_page } from "../../2_views/pages/add_pages/add_reference_page.js";
+import { view_reference_page } from "../../2_views/pages/view_page/view_reference_page.js";
+
+export class controllerReference {
+   static add() {
+      
+   }
+   static view() {
+      
+   }
+}
