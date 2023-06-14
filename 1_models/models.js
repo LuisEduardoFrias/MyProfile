5557272eef738617a8c6c1b478e0f5a3e809f66f
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:09:44 -0400

    update 1_models/models.js

---
+++ 1_models/models.js
@@ -0,0 +1,27 @@
+export class segurity{
+   constructor(){}
+}
+
+export class skills{
+   constructor(){}
+}
+
+export class experiences{
+   constructor(){}
+}
+
+export class proyects{
+   constructor(){}
+}
+
+export class educations{
+   constructor(){}
+}
+
+export class references{
+   constructor(){}
+}
+
+export class all{
+   constructor(){}
+}
