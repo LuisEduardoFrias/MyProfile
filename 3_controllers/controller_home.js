e183c2bd2fa1d13aaceba37a8391efa1e61e87ed
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:31:36 -0400

    update 3_controllers/controller_home.js

---
+++ 3_controllers/controller_home.js
@@ -0,0 +1,11 @@
+import { home_horizontal_page } from "../../2_views/pages/home_horizontal_page.js";
+import { home_vertical_page } from "../../2_views/pages/home_vertical_page.js";
+
+export class controllerHome {
+   horizontal() {
+      
+   }
+   vertical() {
+      
+   }
+}
