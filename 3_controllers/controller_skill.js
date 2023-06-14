4a257d54edd3569c90303772c9680d8f259ccb2e
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:34:28 -0400

    update 3_controllers/controller_skill.js

---
+++ 3_controllers/controller_skill.js
@@ -0,0 +1,11 @@
+import { add_skill_page } from "../../2_views/pages/add_pages/add_skill_page.js";
+import { view_skill_page } from "../../2_views/pages/view_page/view_skill_page.js";
+
+export class controllerSkill {
+   static add() {
+      
+   }
+   static view() {
+      
+   }
+}
