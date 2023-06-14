ba6e97c8af9b58885fd2f7c639f5e891c7cd1f0f
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:13:21 -0400

    update 4_data_access/dataAccess.js

--- 4_data_access/dataAccess.js
+++ 4_data_access/dataAccess.js
@@ -1,23 +1,24 @@
 import data from "./data.json" assert { type: "json" };
+import models from "../1_models/models.js";
 
 export async function getdata(t) {
    return await fetch(base_url + "data/data.json")
       .then((response) => data /*response.json()*/)
       .then((json) => {
          switch (t) {
-            case skills:
+            case models.skills:
                return json.skills;
-            case segurity:
+            case models.segurity:
                return json.segurity;
-            case experiences:
+            case models.experiences:
                return json.experiences;
-            case proyects:
+            case models.proyects:
                return json.proyects;
-            case educations:
+            case models.educations:
                return json.educations;
-            case references:
+            case models.references:
                return json.references;
-            case all:
+            case models.all:
                return json;
             default:
                return undefined;
