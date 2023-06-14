523c773c33d04db4f8a7ba04a946348de02019a6
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:33:57 -0400

    update 3_controllers/controller_experience.js

---
+++ 3_controllers/controller_experience.js
@@ -0,0 +1,11 @@
+import { add_experience_page } from "../../2_views/pages/add_pages/add_experience_page.js";
+import { view_experience_page } from "../../2_views/pages/view_page/view_experience_page.js";
+
+export class controllerExperience {
+   static add() {
+      
+   }
+   static view() {
+      
+   }
+}
