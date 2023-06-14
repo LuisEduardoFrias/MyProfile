9df87943ff4a0094ba660eb78e9953c94c693188
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-14 00:07:23 -0400

    update 3_controllers/controller_skill.js

--- 3_controllers/controller_skill.js
+++ 3_controllers/controller_skill.js
@@ -1,11 +1,14 @@
 import { add_skill_page } from "../../2_views/pages/add_pages/add_skill_page.js";
 import { view_skill_page } from "../../2_views/pages/view_page/view_skill_page.js";
+import { dataAccess } from "../4_data_access/dataAccess.js";
+import { skill } from "../1_models/models.js";
 
 export class controllerSkill {
-   static add() {
-      
-   }
+   static add() {}
    static view() {
-      
+      (async () => {
+         const data = await dataAccess(skill);
+         return view_skill_page(data);
+      })();
    }
 }
