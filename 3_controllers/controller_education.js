fa84bbb1061020cd44c49722a24d5071914a5061
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:30:53 -0400

    update 3_controllers/controller_education.js

---
+++ 3_controllers/controller_education.js
@@ -0,0 +1,11 @@
+import { add_education_page } from "../../2_views/pages/add_pages/add_education_page.js";
+import { view_education_page } from "../../2_views/pages/view_page/view_education_page.js";
+
+export class controllerEducation {
+   add() {
+      
+   }
+   view() {
+      
+   }
+}
