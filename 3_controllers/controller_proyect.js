34dec696f652a511a5f47a4d91033ba7e8f4be20
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-13 23:36:25 -0400

    update 3_controllers/controller_proyect.js

--- 3_controllers/controller_proyect.js
+++ 3_controllers/controller_proyect.js
@@ -2,10 +2,10 @@ import { add_proyect_page } from "../../2_views/pages/add_pages/add_proyect_page
 import { view_proyect_page } from "../../2_views/pages/view_page/view_proyect_page.js";
 
 export class controllerProyect {
-   add() {
+   static add() {
       
    }
-   view() {
+   static view() {
       
    }
 }
