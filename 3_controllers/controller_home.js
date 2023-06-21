7e203eb00027933f8bbf4b472988f600a3354968
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:16:11 -0400

    update 3_controllers/controller_home.js

--- 3_controllers/controller_home.js
+++ 3_controllers/controller_home.js
@@ -1,11 +1,12 @@
-import { home_horizontal_page } from "../../2_views/pages/home_horizontal_page.js";
-import { home_vertical_page } from "../../2_views/pages/home_vertical_page.js";
+import home_horizontal_page from "../2_views/pages/home_horizontal_page.js";
+import home_vertical_page from "../2_views/pages/home_vertical_page.js";
+import { all } from "../1_models/models.js";
 
-export class controllerHome {
-   horizontal() {
-      
+export default class controller_home {
+   static horizontal(gate) {
+      home_horizontal_page(gate);
    }
-   vertical() {
-      
+   static vertical(gate) {
+      return (async () => home_vertical_page(await gate.get(all)))();
    }
 }
