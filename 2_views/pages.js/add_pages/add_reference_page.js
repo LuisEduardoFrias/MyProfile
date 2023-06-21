3dee105b0123544533a395d352dce3f9de1a5c98
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:32:15 -0400

    update 2_views/pages.js/add_pages/add_reference_page.js

--- 2_views/pages.js/add_pages/add_reference_page.js
+++ 2_views/pages.js/add_pages/add_reference_page.js
@@ -1,35 +1,58 @@
-import { Ui } from "../../../3_controllers/helpers/ui.js";
-import { handleUrl } from "../../../3_controllers/helpers/handleUrl.js";
-import { controller_reference } from "../../../3_controllers/controller_";
+import Ui from "../../../3_controllers/helpers/ui.js";
+import handleUrl from "../../../3_controllers/helpers/handleUrl.js";
+import controller_reference from "../../../3_controllers/controller_reference.js";
 
-
-export default function addReference() {
+export default function add_reference_page() {
+   //
    const { Div, Label, Button, Input, Form } = Ui;
-   return Div([
+   //
+   return Form([
       Div(
-            [Button("back", ".back-button", null, (e) => handleUrl.back())],
-            ".view-container-back-btn"
-         ),
-      Form(
          [
             Div(
                [
-                  Label("add new refecence", ".tittle-section"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Button(
-                     "save",
-                     ".btn-section",
-                     null,
-                     (e) =>  controller_reference.post()
+                  Button("back", ".back-btn", null, (e) => {
+                     handleUrl.back();
+                  }),
+                  Label("Add New Reference", ".add-page-tittle-page"),
+               ],
+               ".add-page-container-back-btn-tittle"
+            ),
+
+            Div(
+               [
+                  Div(
+                     [
+                        Input(
+                           null,
+                           "Name",
+                           true,
+                           null,
+                           "error",
+                           "Name",
+                           "fild"
+                        ),
+                        Input(
+                           null,
+                           "Phone Number",
+                           true,
+                           null,
+                           "error",
+                           "PhoneNumber",
+                           "fild"
+                        ),
+                     ],
+                     ".add-page-filds"
+                  ),
+
+                  Button("Save", ".save-btn", null, (e) =>
+                     controller_reference.post(da, document.forms[0])
                   ),
                ],
-               ".section"
+               ".add-page-form form-reference"
             ),
          ],
-         ".setting-container-form"
+         ".add-page-container-page"
       ),
    ]);
 }
