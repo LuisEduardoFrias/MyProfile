a7fcc83bf5a7eecbf001e164ea2ab90112bfdbb4
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:17:46 -0400

    update 3_controllers/controller_skill.js

--- 3_controllers/controller_skill.js
+++ 3_controllers/controller_skill.js
@@ -1,16 +1,35 @@
-import { add_skill_page } from "../../2_views/pages/add_pages/add_skill_page.js";
-import { view_skill_page } from "../../2_views/pages/view_page/view_skill_page.js";
-import { dataAccess } from "../4_data_access/dataAccess.js";
-import { skill } from "../1_models/models.js";
+import add_skill_page from "../2_views/pages/add_pages/add_skill_page.js";
+import view_skill_page from "../2_views/pages/view_pages/view_skill_page.js";
+//
+import handleUrl from "./helpers/handleUrl.js";
+import { Mapper, clearForm } from "./helpers/tools.js";
+import { skills } from "../1_models/models.js";
 
-export class controllerSkill {
-   static add() {
-      return add_skill_page;
+export default class controller_skill {
+   static add(gate) {
+      return add_skill_page(gate);
    }
-   static view() {
+   static view(gate) {
+      return (async () => {
+         return view_skill_page(await gate.get(skills), gate);
+      })();
+   }
+   static post(gate, form) {
       (async () => {
-         const data = await dataAccess(skill);
-         return view_skill_page(data);
+         const res = await gate.post(Mapper(form, skills));
+         if (res) {
+            clearForm(form);
+         }
+         alert("data save");
+      })();
+   }
+   static delete(gate, value) {
+      (async () => {
+         const res = await gate.delete(new skills(), value);
+         alert("data delete");
+         handleUrl.changePage({
+            target: { href: "/view/skills" },
+         });
       })();
    }
 }
