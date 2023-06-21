074232105d8815746243386548c7555b9849cef1
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:32:59 -0400

    update 2_views/pages.js/add_pages/add_skill_page.js

--- 2_views/pages.js/add_pages/add_skill_page.js
+++ 2_views/pages.js/add_pages/add_skill_page.js
@@ -1,34 +1,59 @@
-import { Ui } from "../../../3_controllers/helpers/ui.js";
-import { handleUrl } from "../../../3_controllers/helpers/handleUrl.js";
-import { controller_skill } from "../../../3_controllers/controller_";
+import Ui from "../../../3_controllers/helpers/ui.js";
+import handleUrl from "../../../3_controllers/helpers/handleUrl.js";
+import controller_skill from "../../../3_controllers/controller_skill.js";
 
-export default function addSkill() {
-   const { Div, Label, Button, Input, Form } = Ui;
-   return Div([
+export default function add_skill_page(da) {
+   //
+   const { Div, Label, Button, Input, Form, Select } = Ui;
+   //
+   return Form([
       Div(
-         [Button("back", ".back-button", null, (e) => handleUrl.back())],
-         ".view-container-back-btn"
-      ),
-      Form(
          [
             Div(
                [
-                  Label("add new skill", ".tittle-section"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Button(
-                     "save",
-                     ".btn-section",
-                     null,
-                     (e) =>  controller_skill.post()
-                  ),
+                  Button("back", ".back-btn", null, (e) => {
+                     handleUrl.back();
+                  }),
+                  Label("Add New Skill", ".add-page-tittle-page"),
                ],
-               ".section"
+               ".add-page-container-back-btn-tittle"
+            ),
+
+            Div(
+               [
+                  Div(
+                     [
+                        Input(
+                           null,
+                           "Name",
+                           true,
+                           null,
+                           "error",
+                           "Name",
+                           "fild"
+                        ),
+                        Input(
+                           null,
+                           "Image name",
+                           true,
+                           "file",
+                           "error",
+                           "ImageName",
+                           "fild"
+                        ),
+                        
+                     ],
+                     ".add-page-filds"
+                  ),
+
+                  Button("Save", ".save-btn hadd-page-save-btn", null, (e) => {
+                     controller_skill.post(da, document.forms[0]);
+                  }),
+               ],
+               ".add-page-form form-skill"
             ),
          ],
-         ".setting-container-form"
+         ".add-page-container-page"
       ),
    ]);
 }
