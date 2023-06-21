4390f055fff238ba23f4e9079807a067dc41c92d
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:15:16 -0400

    update 3_controllers/controller_education.js

--- 3_controllers/controller_education.js
+++ 3_controllers/controller_education.js
@@ -1,11 +1,32 @@
-import { add_education_page } from "../../2_views/pages/add_pages/add_education_page.js";
-import { view_education_page } from "../../2_views/pages/view_page/view_education_page.js";
+import add_education_page from "../../2_views/pages/add_pages/add_education_page.js";
+import view_education_page from "../../2_views/pages/view_pages/view_education_page.js";
+//
+import { Mapper } from "./helpers/tools.js";
+import { educations } from "../1_models/models.js";
 
-export class controllerEducation {
-   static add() {
-      
+export default class controller_education {
+   static add(gate) {
+      return add_education_page(gate);
    }
-   static view() {
-      
+   static view(gate) {
+      return (async () => {
+         return view_education_page(await gate.get(educations), gate);
+      })();
+   }
+   static post(gate, form) {
+      (async () => {
+         const res = await gate.post(Mapper(form, educations));
+         if (res) {
+            clearForm(form);
+         }
+         alert("data save");
+      })();
+   }
+   static delete(gate, value) {
+      (async () => {
+         const res = await gate.delete(value);
+         alert("data delete");
+         this.view(gate);
+      })();
    }
 }
