c4ea5a5767d84cb82bc1f56152242daefbe2744d
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:16:32 -0400

    update 3_controllers/controller_proyect.js

--- 3_controllers/controller_proyect.js
+++ 3_controllers/controller_proyect.js
@@ -1,11 +1,32 @@
-import { add_proyect_page } from "../../2_views/pages/add_pages/add_proyect_page.js";
-import { view_proyect_page } from "../../2_views/pages/view_page/view_proyect_page.js";
+import add_proyect_page from "../../2_views/pages/add_pages/add_proyect_page.js";
+import view_proyect_page from "../../2_views/pages/view_pages/view_proyect_page.js";
+//
+import { Mapper } from "./helpers/tools.js";
+import { proyects } from "../1_models/models.js";
 
-export class controllerProyect {
-   static add() {
-      
+export default class controller_proyect {
+   static add(gate) {
+      return add_proyect_page(gate);
    }
-   static view() {
-      
+   static view(gate) {
+      return (async () => {
+         return view_proyect_page(await gate.get(proyects), gate);
+      })();
+   }
+   static post(gate, form) {
+      (async () => {
+         const res = await gate.post(Mapper(form, proyects));
+         if (res) {
+            clearForm(form);
+         }
+         alert("data save");
+      })();
+   }
+   static delete(gate, value) {
+      (async () => {
+         const res = await gate.delete(value);
+         alert("data delete");
+         this.view(gate);
+      })();
    }
 }
