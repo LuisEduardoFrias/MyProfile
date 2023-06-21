0506824150eb50b1cff3b4137a44417732c56bdd
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:33:34 -0400

    update 2_views/pages.js/view_page/view_education_page.js

--- 2_views/pages.js/view_page/view_education_page.js
+++ 2_views/pages.js/view_page/view_education_page.js
@@ -1,31 +1,33 @@
-import { ui } from "../../../1_controllers/js/ui.js";
-import { handleUrl } from "../../../1_controllers/js/handleUrl.js";
+import Ui from "../../../3_controllers/helpers/ui.js";
+import handleUrl from "../../../3_controllers/helpers/handleUrl.js";
+import controller_education from "../../../3_controllers/controller_education.js";
 
-export default function viewEducation(data) {
+export default function view_education_page(data, da) {
    const { Div, ViewData, Label, Button } = Ui;
 
    return Div(
       [
          Div(
             [
-               Label("reference", ".tittle-page education-page-tittle"),
+               Label("Educations", ".view-tittle-page education-page-tittle"),
                ViewData(
                   data,
                   () => {
-                     handleUrl.changePageEvenClick({
+                     handleUrl.changePage({
                         target: { href: "/add/education" },
                      });
                   },
-                  (key) => {
-                     // skill.key = key;
-                     /*  gate.delete((e) => alert(e), skill);*/
-                  }
+                  (value, e) => controller_education.delete(da, value)
                ),
             ],
             ".view-container-data education-container-viewproyect"
          ),
          Div(
-            [Button("back", ".back-button", null, (e) => handleUrl.back())],
+            [
+               Button("back", ".back-btn", null, (e) => {
+                  handleUrl.back();
+               }),
+            ],
             ".view-container-back-btn"
          ),
       ],
