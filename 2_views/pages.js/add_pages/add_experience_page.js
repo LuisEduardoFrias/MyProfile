c11a9d87d31ec7610cac9933130438ce2a93bddd
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:30:16 -0400

    update 2_views/pages.js/add_pages/add_experience_page.js

--- 2_views/pages.js/add_pages/add_experience_page.js
+++ 2_views/pages.js/add_pages/add_experience_page.js
@@ -1,35 +1,97 @@
-import { Ui } from "../../../3_controllers/helpers/ui.js";
-import { handleUrl } from "../../../3_controllers/helpers/handleUrl.js";
-import { controller_experence } from "../../../3_controllers/controller_";
+import Ui from "../../../3_controllers/helpers/ui.js";
+import handleUrl from "../../../3_controllers/helpers/handleUrl.js";
+import controller_experience from "../../../3_controllers/controller_experience.js";
+import { Select } from "../../../3_controllers/helpers/tools.js";
 
-
-export default function addExperience() {
+export default function add_experience_page() {
+   //
    const { Div, Label, Button, Input, Form } = Ui;
-   return Div([
+   let countTask = 1;
+   //
+   return Form([
       Div(
-            [Button("back", ".back-button", null, (e) => handleUrl.back())],
-            ".view-container-back-btn"
-         ),
-      Form(
          [
             Div(
                [
-                  Label("add new experience", ".tittle-section"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Input("", "placeholder", "t", "error", "name"),
-                  Button(
-                     "save",
-                     ".btn-section",
-                     null,
-                     (e) =>  controller_reference.post()
+                  Button("back", ".back-btn", null, (e) => {
+                     handleUrl.back();
+                  }),
+                  Label("Add New Experience", ".add-page-tittle-page"),
+               ],
+               ".add-page-container-back-btn-tittle"
+            ),
+
+            Div(
+               [
+                  Div(
+                     [
+                        Input("", "Company", "text", "error", "Company", "fild"),
+                        Input(
+                           "",
+                           "Description",
+                           "text",
+                           "error",
+                           "Description",
+                           "fild"
+                        ),
+                        Input("", "Position", "text", "error", "Position", "fild"),
+
+                        Div(
+                           [
+                              Div(
+                                 [
+                                    Label("Task", ".add-page-tittle-task"),
+                                    Ui.Button(
+                                       Ui.Icon("add_circle", "icon-switch"),
+                                       ".btn-add-fild",
+                                       null,
+                                       (e) => {
+                                          const divfather = Select(".task");
+
+                                          divfather.appendChild(
+                                             Input(
+                                                null,
+                                                "New task",
+                                                "text",
+                                                "error",
+                                                `Task-Name-${countTask}`,
+                                                "fild"
+                                             )
+                                          );
+                                          countTask++;
+                                       }
+                                    ),
+                                 ],
+                                 ".add-btn-header"
+                              ),
+                              Div(
+                                 [
+                                    Input(
+                                       null,
+                                       "New task",
+                                       "t",
+                                       "error",
+                                       "Tern-Name-0",
+                                       "fild"
+                                    ),
+                                 ],
+                                 ".task add-item-group"
+                              ),
+                           ],
+                           ".task-group"
+                        ),
+                     ],
+                     ".add-page-filds"
+                  ),
+
+                  Button("Save", ".save-btn", null, (e) =>
+                     controller_experience.post(da, document.forms[0])
                   ),
                ],
-               ".section"
+               ".add-page-form form-experience"
             ),
          ],
-         ".setting-container-form"
+         ".add-page-container-page"
       ),
    ]);
 }
