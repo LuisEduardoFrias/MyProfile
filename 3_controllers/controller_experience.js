693935cc7d33e9706249861a4671041069f30bc4
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:15:39 -0400

    update 3_controllers/controller_experience.js

--- 3_controllers/controller_experience.js
+++ 3_controllers/controller_experience.js
@@ -1,11 +1,32 @@
-import { add_experience_page } from "../../2_views/pages/add_pages/add_experience_page.js";
-import { view_experience_page } from "../../2_views/pages/view_page/view_experience_page.js";
+import add_experience_page from "../../2_views/pages/add_pages/add_experience_page.js";
+import view_experience_page from "../../2_views/pages/view_pages/view_experience_page.js";
+//
+import { Mapper } from "./helpers/tools.js";
+import { experiences } from "../1_models/models.js";
 
-export class controllerExperience {
-   static add() {
-      
+export default class controller_experience {
+   static add(gate) {
+      return add_experience_page(gate);
    }
-   static view() {
-      
+   static view(gate) {
+      return (async () => {
+         return view_experience_page(await gate.get(experiences), gate);
+      })();
+   }
+   static post(gate, form) {
+      (async () => {
+         const res = await gate.post(Mapper(form, experiences));
+         if (res) {
+            clearForm(form);
+         }
+         alert("data save");
+      })();
+   }
+   static delete(gate, value) {
+      (async () => {
+         const res = await gate.delete(value);
+         alert("data delete");
+         this.view(gate);
+      })();
    }
 }
