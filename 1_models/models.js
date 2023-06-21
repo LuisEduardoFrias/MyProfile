c9c970a6a959f0a0e3c7896f6923cfed47b2fec5
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:02:06 -0400

    update 1_models/models.js

--- 1_models/models.js
+++ 1_models/models.js
@@ -1,27 +1,42 @@
-export class segurity{
-   constructor(){}
+export class segurity {
+   constructor() {}
 }
 
-export class skills{
-   constructor(){}
+export class skills {
+   constructor(Name,ImageName) {
+      this.Name = Name;
+      this.ImageName = ImageName;
+   }
 }
 
-export class experiences{
-   constructor(){}
+export class experiences {
+   constructor() {}
 }
 
-export class proyects{
-   constructor(){}
+export class proyects {
+   constructor() {}
 }
 
-export class educations{
-   constructor(){}
+export class educations {
+   constructor() {}
 }
 
-export class references{
-   constructor(){}
+export class references {
+   constructor() {}
 }
 
-export class all{
-   constructor(){}
+export class all {
+   constructor() {}
 }
+
+class models {}
+
+models.segurity = segurity;
+models.skills = skills;
+models.experiences = experiences;
+models.proyects = proyects;
+models.educations = educations;
+models.references = references;
+models.all = all;
+
+export default models;
