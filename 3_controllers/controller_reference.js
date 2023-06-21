0d4ad34ff4392eb06654bde69245172681f74d8e
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-21 19:16:57 -0400

    update 3_controllers/controller_reference.js

--- 3_controllers/controller_reference.js
+++ 3_controllers/controller_reference.js
@@ -1,11 +1,32 @@
-import { add_reference_page } from "../../2_views/pages/add_pages/add_reference_page.js";
-import { view_reference_page } from "../../2_views/pages/view_page/view_reference_page.js";
+import add_reference_page from "../../2_views/pages/add_pages/add_reference_page.js";
+import view_reference_page from "../../2_views/pages/view_pages/view_reference_page.js";
+//
+import { Mapper } from "./helpers/tools.js";
+import { references } from "../1_models/models.js";
 
-export class controllerReference {
-   static add() {
-      
+export default class controller_reference {
+   static add(gate) {
+      return add_reference_page(gate);
    }
-   static view() {
-      
+   static view(gate) {
+      return (async () => {
+         return view_reference_page(await gate.get(references), gate);
+      })();
+   }
+   static post(gate, form) {
+      (async () => {
+         const res = await gate.post(Mapper(form, references));
+         if (res) {
+            clearForm(form);
+         }
+         alert("data save");
+      })();
+   }
+   static delete(gate, value) {
+      (async () => {
+         const res = await gate.delete(value);
+         alert("data delete");
+         this.view(gate);
+      })();
    }
 }
